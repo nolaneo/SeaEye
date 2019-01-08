c03ba0a0ab85d1aea92f3d7e9445a00e4e704e8b
Conor Mongey <conor@mongey.net>
2019-01-08 14:49:54 +0000

    Create listeners group, extract text printer

--- SeaEye/services/ClientBuildUpdater.swift
+++ SeaEye/services/ClientBuildUpdater.swift
@@ -5,12 +5,6 @@ protocol BuildUpdateListener {
     mutating func notify(project: Project, builds: [CircleCIBuild])
 }
 
-struct TextPrinter: BuildUpdateListener {
-    func notify(project: Project, builds: [CircleCIBuild]) {
-        print("\(project) got new \(builds.count) builds")
-    }
-}
-
 // Notifies the listeners with *new* builds that the user cares about for a project, from a buildClient
 // on first run it will return all known builds, subsequent notificates will only show updated builds.
 class ClientBuildUpdater {
